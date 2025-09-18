e880acf54bc7f0462ee6fb5beb1616d11b5e3188
Jorsch-0 <jorge.leon@jortech.cc>
2025-09-17 19:27:52 -0500

    feat

--- src/main.ts
+++ src/main.ts
@@ -24,6 +24,11 @@ async function bootstrap() {
 
   app.use(cookieParser());
 
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  });
+
   await app.listen(env.PORT);
 }
 void bootstrap();
